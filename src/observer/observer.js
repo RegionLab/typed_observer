2772ed855485b1cf1cd191dfb6176d20d0a7031a
Andrey Shchetinin <schetinin1993@gmail.com>
2016-04-12 17:49:44 +0600

    ...

--- src/observer/observer.js
+++ src/observer/observer.js
@@ -1,17 +1,35 @@
-import _ from 'lodash';
+import {
+    isObject,
+    isFunction,
+    each
+} from 'lodash';
 
 export default class Observer {
     constructor() {
-
+        this.dataValues = {};
+        this.data = {};
+        this.updateCbs = [];
+        this.locks = [];
+        this.filterVersion = 0;
     }
 
     define(name, type, customValidation) {
+        if(name && isObject(type)) {
 
+        }
+    }
+
+    onUpdate(name, cb) {
+        if(isFunction(name)) {
+            cb = name;
+        }
     }
 
     extendUpdate(name, cb = false) {
-        if(!cb && _.isFunction(name)) {
-
+        if(!cb && isFunction(name)) {
+            Object.defineProperty(this.data, {
+                configurable: true,
+            })
         }
     }
 
